0b9dcc67c0a310fa73d3f202f53bf44b04001e7f
Bruno Bertolini <bruno.bertolini@hotmail.com>
2021-07-16 02:23:39 -0300

    Data/Room classes implemented

--- app/src/main/java/br/com/brunoccbertolini/businesscard/App.kt
+++ app/src/main/java/br/com/brunoccbertolini/businesscard/App.kt
@@ -1,7 +1,10 @@
 package br.com.brunoccbertolini.businesscard
 
 import android.app.Application
+import br.com.brunoccbertolini.businesscard.data.AppDatabase
+import br.com.brunoccbertolini.businesscard.data.BusinessCardRepository
 
 class App: Application() {
-
+    val database by lazy { AppDatabase.getDatabase(this) }
+    val repository by lazy { BusinessCardRepository(database.businessDao()) }
 }
